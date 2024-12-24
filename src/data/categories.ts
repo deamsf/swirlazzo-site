f94ed98a4b25abe31fb75a16e2ad2d775d8782ce
deamsf <frankdamen+stackblitz@gmail.com>
2024-12-24 20:23:14 +0100

    image bugfix but no slider?

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -2,15 +2,15 @@ import type { Category } from '../types/category';
 
 export const featuredCategories: Category[] = [
   {
-    name: "New Arrivalsxy",
-    image: "https://img.freepik.com/free-photo/beautiful-hand-made-objects_23-2148732441.jpg",
+    name: "New Arrivals",
+    image: '/img/coll/item6.jpg',
     icon: "FaStar",
     href: "https://www.swirlazzo.be/shop/new-arrivals",
     featured: true
   },
   {
     name: "Best Sellers",
-    image: "https://img.freepik.com/free-photo/decorative-vase-with-dried-flowers_23-2149835514.jpg",
+    image: "/img/coll/item1.jpg",
     icon: "FaRocket",
     href: "https://www.swirlazzo.be/shop/best-sellers",
     featured: true
@@ -20,32 +20,32 @@ export const featuredCategories: Category[] = [
 export const categories: Category[] = [
   { 
     name: 'Jars & Containers', 
-    image: './src/img/coll/item1.jpg',
+    image: '/img/coll/item1.jpg',
     href: 'https://www.swirlazzo.be/shop/jars'
   },
   { 
     name: 'Card Holders', 
-    image: './src/img/coll/item4.jpg',
+    image: '/img/coll/item4.jpg',
     href: 'https://www.swirlazzo.be/shop/card-holders'
   },
   { 
     name: 'Decorative Trays', 
-    image: './src/img/coll/item3.jpg',
+    image: '/img/coll/item3.jpg',
     href: 'https://www.swirlazzo.be/shop/decorative-trays'
   },
   { 
     name: 'Coasters', 
-    image: './src/img/coll/item2.jpg',
+    image: '/img/coll/item2.jpg',
     href: 'https://www.swirlazzo.be/shop/coasters'
   },
   { 
     name: 'Wall Art', 
-    image: './src/img/coll/item5.jpg',
+    image: '/img/coll/item5.jpg',
     href: 'https://www.swirlazzo.be/shop/wall-art'
   },
   { 
     name: 'Custom Pieces', 
-    image: './src/img/coll/item6.jpg',
+    image: '/img/coll/item6.jpg',
     href: 'https://www.swirlazzo.be/shop/custom-pieces'
   }
 ];
